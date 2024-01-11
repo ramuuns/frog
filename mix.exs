da5c6d494b5af03b3b32d73d5b4ae8aa78d59c12
Ramūns Usovs <ramuuns@enkurs.org>
2024-01-11 07:31:22 +0100

    Update events to 0.2.1
    This fixes an issue that was spotted in aura prod and made us lose
    events

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Frog.MixProject do
     [
       {:phoenix, "~> 1.6.15"},
       {:phoenix_ecto, "~> 4.4"},
-      {:phoenix_events, ">= 0.1.3"},
+      {:phoenix_events, ">= 0.2.1"},
       {:ecto_sql, "~> 3.11"},
       {:ecto_sqlite3, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
